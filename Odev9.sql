594811e4e734aaa42faf57d2d9bd5e191812c8aa
Olcay Açıkdeniz <107851080+olcayacikdeniz@users.noreply.github.com>
2022-06-25 20:48:33 +0300

    Create Odev9.sql

---
+++ Odev9.sql
@@ -0,0 +1,12 @@
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+Select city, country from country inner join city on city.country_id = country.country_id;
+
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+Select payment_id, first_name, last_name  from payment inner join customer on customer.customer_id = payment.customer_id;
+
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+
+Select rental_id, first_name, last_name  from rental inner join customer on customer.customer_id = rental.customer_id;
