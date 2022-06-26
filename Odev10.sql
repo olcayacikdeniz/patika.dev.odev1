6765a27828c4da4ebac50494322c6b14bc291fbc
Olcay Açıkdeniz <107851080+olcayacikdeniz@users.noreply.github.com>
2022-06-26 11:53:07 +0300

    Update Odev10.sql

--- Odev10.sql
+++ Odev10.sql
@@ -6,4 +6,7 @@ Select city, country from city left join country on city.country_id = country.co
 
 Select payment_id, first_name, last_name  from customer right join payment on customer.customer_id = payment.customer_id;
 
+
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz FULL JOIN sorgusunu yazınız.
+
 Select rental_id, first_name, last_name  from customer full join rental on customer.customer_id = rental.customer_id;
